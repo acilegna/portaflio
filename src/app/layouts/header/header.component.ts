139b0f51d9c63840716eb19f77144ed1b56d34c0
acilegna <acilegna.airam88@gmail.com>
2024-06-06 22:47:36 -0600

    avance responsive

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,32 +1,36 @@
 import {
-  AfterViewInit, Inject, PLATFORM_ID,
+  AfterViewInit,
+  Inject,
+  PLATFORM_ID,
   Component,
   ElementRef,
   OnInit,
   ViewChild,
+  ViewChildren,
+  QueryList,
 } from '@angular/core';
-import { statech,translateAnimation,fadeSequen,fadeGroup   } from 'src/app/animations';
+
+import {
+  statech,
+  translateAnimation,
+  fadeSequen,
+  fadeGroup,
+} from 'src/app/animations';
+
 import { gsap } from 'gsap';
 import { isPlatformBrowser } from '@angular/common';
-import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
-
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  animations: [statech,translateAnimation,fadeSequen,  fadeGroup   ],
+  animations: [statech, translateAnimation, fadeSequen, fadeGroup],
 })
 
 export class HeaderComponent implements AfterViewInit {
-  @ViewChild('about', { static: true }) buttonAbout: ElementRef<HTMLHeadElement>;
-  @ViewChild('portfolio', { static: true }) buttonTrabajo: ElementRef<HTMLHeadElement>;
-  @ViewChild('home', { static: true }) buttonHome: ElementRef<HTMLHeadElement>;
-  @ViewChild('contact', { static: true }) buttonContact: ElementRef<HTMLHeadElement>;
-  @ViewChild('social', { static: true }) buttonsocialt: ElementRef<HTMLHeadElement>;
-  @ViewChild('sociall', { static: true }) buttonsocial: ElementRef<HTMLHeadElement>;
+  @ViewChildren('menu') buttonsocialt: QueryList<ElementRef>;
 
   @ViewChild('section', { static: true }) section: ElementRef<HTMLHeadElement>;
   @ViewChild('input', { static: true }) Input: ElementRef<HTMLInputElement>;
@@ -35,10 +39,7 @@ export class HeaderComponent implements AfterViewInit {
   //valor a recibir
   public padre = 'papa';
 
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-
-  }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   sequencesGroup = true;
   sequences = true;
 
@@ -53,75 +54,66 @@ export class HeaderComponent implements AfterViewInit {
     // manejador del evento
     let foo = this.isCollapse;
     this.isCollapse = foo === false ? true : false;
-
   }
 
   onTrigger(id: any) {
     this.padre = id;
 
     if (this.padre == 'home') {
-      this.styleRed()
+      this.styleRed();
     }
 
     if (this.padre == 'about') {
-      this.stylePink()
+      this.stylePink();
     }
 
     if (this.padre == 'portfolio') {
-      this.styleBlue()
+      this.styleBlue();
     }
   }
 
   styleBlue() {
-    this.buttonHome.nativeElement.setAttribute('style', 'color:#0c9693;');
-    this.buttonAbout.nativeElement.setAttribute('style', 'color:#0c9693;');
-    this.buttonContact.nativeElement.setAttribute('style', 'color:#0c9693;');
-    this.buttonsocial.nativeElement.setAttribute('style', 'color:#0c9693;');
-    this.buttonsocialt.nativeElement.setAttribute('style', 'color:#0c9693;');
-
+    let toArray = this.buttonsocialt.toArray();
+    let ref = toArray.find((el) =>
+      el.nativeElement.setAttribute('style', 'color:#0c9693')
+    );
   }
 
   styleRed() {
-    this.buttonAbout.nativeElement.setAttribute('style', 'color:#919191;');
-    this.buttonTrabajo.nativeElement.setAttribute('style', 'color:#919191;');
-    this.buttonContact.nativeElement.setAttribute('style', 'color:#919191;');
-    this.buttonsocial.nativeElement.setAttribute('style', 'color:#919191;');
-    this.buttonsocialt.nativeElement.setAttribute('style', 'color:#919191;');
+    let toArray = this.buttonsocialt.toArray();
+    let ref = toArray.find((el) =>
+      el.nativeElement.setAttribute('style', 'color:#919191')
+    );
   }
 
   stylePink() {
-    this.buttonTrabajo.nativeElement.setAttribute('style', 'color: #e349c4;');
-    this.buttonHome.nativeElement.setAttribute('style', 'color: #e349c4;');
-    this.buttonContact.nativeElement.setAttribute('style', 'color: #e349c4;');
-    this.buttonsocial.nativeElement.setAttribute('style', 'color: #e349c4;');
-    this.buttonsocialt.nativeElement.setAttribute('style', 'color: #e349c4;');
+    let toArray = this.buttonsocialt.toArray();
+    let ref = toArray.find((el) =>
+      el.nativeElement.setAttribute('style', 'color: #e349c4')
+    );
   }
   /*   Fíjate que tienes casi lo mismo, pero le pides a Angular que te inyecte el tipo de plataforma
     (@Inject(PLATFORM_ID) private platformId: Object), y de este modo, antes de suscribirte a windows, 
     compruebas si efectivamente te encuentras en el navegador con isPlatformBrowser(this.platformId). */
 
-
   ngAfterViewInit(): void {
     gsap.registerPlugin(ScrollTrigger);
 
     if (isPlatformBrowser(this.platformId)) {
-
-      gsap.to(".sublogo", {
-        x: "-52vw", // mover 32 a la izquierda, positivo ala derecha
-        y: "15vh",
-        scale: 2.5,
+      gsap.to('.sublogo', {
+        x: '-38vw', // mover 32 a la izquierda, positivo ala derecha
+        y: '17.3vh',
+        scale: 3.5,
         rotate: 360,
         scrollTrigger: {
-          trigger: ".sublogo", //elemento animar
-          markers: true,
-          start: "top 80%",
-          end: "top 40%",
-          scrub: 3, // 
-
-        }
+          trigger: '.sublogo', //elemento animar
+          //markers: true,
+  
+          start: 'top 120%',
+          end: 'top 40%',
+          scrub: 3, //
+        },
       });
     }
   }
-
 }
-
