5f0542f8166803cd243a21b61ece7008a3f6a129
acilegna <acilegna.airam88@gmail.com>
2024-07-17 23:26:25 -0600

    email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
+ 
+ 
+import { HttpClientModule } from '@angular/common/http';
+ 
+ 
+import { MessageService } from './services/message.service';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -34,6 +41,7 @@ import { ContactComponent } from './pages/contact/contact.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -57,7 +65,7 @@ import { ContactComponent } from './pages/contact/contact.component';
       },
     ]), */
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,38 @@
 import { Component, Input } from '@angular/core';
-import {  translateAnimation,Animation, translates,hijo2,hijo3,container} from 'src/app/animations';
+import {
+  translateAnimation,
+  Animation,
+  translates,
+  hijo2,
+  hijo3,
+  container,
+} from 'src/app/animations';
+import { MessageService } from 'src/app/services/message.service';
+
+//import * as swal from 'sweetalert';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
- 
-  animations: [translateAnimation, Animation,   translates,hijo2,hijo3, container],
-})
 
+  animations: [
+    translateAnimation,
+    Animation,
+    translates,
+    hijo2,
+    hijo3,
+    container,
+  ],
+})
 export class ContactComponent {
+  constructor(public _MessageService: MessageService) {}
   @Input() hijo = '';
-  
+  contactForm(form: any) {
+    this._MessageService.sendMessage(form).subscribe(() => {
+    //swal.fire("Formulario de contacto", "Mensaje enviado correctamente", 'success');
+    });
+    }
+
 }
