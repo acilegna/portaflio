5a3bdb4f84044ddf35a76594a8cd1bec521077c7
acilegna <acilegna.airam88@gmail.com>
2024-07-27 22:00:07 -0600

    url post

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -8,9 +8,8 @@ export class MessageService {
   constructor(private _http: HttpClient) { }
   sendMessage(body: any) {
 // return this._http.post('http://localhost:3000/formulario', body); 
- return this._http.post('https://nodbackemail.vercel.app/formulario', body); 
+ return this._http.post('https://backemail.vercel.app/formulario', body); 
    }
 }
 
- 
  
