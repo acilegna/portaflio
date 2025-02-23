1b5c61c7303da0b107f2fa360326bb8022089277
deleonconstruccion <139820753+deleonconstruccion@users.noreply.github.com>
2025-02-23 00:20:28 -0600

    update cv

--- src/app/directivas/InterObserver.directive.ts
+++ src/app/directivas/InterObserver.directive.ts
@@ -7,45 +7,41 @@ import {
 } from '@angular/core';
 import { HeaderComponent } from '../layouts/header/header.component';
 
-
 @Directive({
   selector: '[intersection]',
 })
 export class InterObserver implements OnInit, AfterViewInit {
   @Output() trigger = new EventEmitter();
- 
-  constructor(private header: HeaderComponent) { }
+
+  constructor(private header: HeaderComponent) {}
 
   ngAfterViewInit() {
     this.change();
   }
 
   ngOnInit() {
-  // window.location.replace('#home');
+    // window.location.replace('#home');
   }
 
   change() {
     const changeNav = (entries: any) => {
       entries.forEach((entry: any) => {
         // verificar el elemnto que esta siendo intersectado c
-       
-        if (entry.isIntersecting      ) {
+
+        if (entry.isIntersecting) {
           // encontrar los elements (<a> ) que contenga clase activa y eliminarla
           document.querySelector('.activa').classList.remove('activa');
 
           //obteber id de la seccion que esta siendo intersectada
           var id = entry.target.getAttribute('id');
-          
-    
+
           /*  this.header.buttonAbout.nativeElement.setAttribute('style', 'color:#0c9693;');
         
            this.header.buttons.nativeElement.setAttribute('style', 'color:#0c9693;'); */
 
-
           this.trigger.emit(id);
 
-         // window.location.replace('#' + id);
-
+          // window.location.replace('#' + id);
 
           //encontrar la etiqueta del menu que coincida con el id de la seccion intersectada  y agregar clase "activa"
           var newLink = document
@@ -53,11 +49,10 @@ export class InterObserver implements OnInit, AfterViewInit {
             .classList.add('activa');
         }
       });
-      
     };
 
     const options = {
-      threshold:0.95,
+      threshold: 0.95,
     };
 
     const observer = new IntersectionObserver(changeNav, options);
